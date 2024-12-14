cc7fc793714b5e1f59ef437c88d22c3832931537
Denis Bochkarev <bochkarevdenis1995@gmail.com>
2024-12-14 18:16:40 +0400

    add docker compose

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -101,9 +101,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = BASE_DIR / 'backend_static'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
