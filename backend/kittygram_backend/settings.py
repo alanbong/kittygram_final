8785e464dccbe1f13b804a1b1d7fbd9d02cba8a2
Denis Bochkarev <bochkarevdenis1995@gmail.com>
2024-12-19 00:14:34 +0400

    test

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -95,15 +95,15 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
+ 
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = 'backend_static'
 
 MEDIA_URL = '/media/'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = 'backend_media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
